33166fd967f5757337a8450c8b51a30adff71fe2
Mystic Ninja <125064688+mysticnin@users.noreply.github.com>
2023-06-15 15:07:24 -0500

    Update layout.tsx
    dev/header

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,6 +19,20 @@ export default function Layout({ children }: LayoutProps) {
           {children}
         </main>
       </div>
+    <header className="container sticky top-0 z-40 bg-white">
+        <div className="h-16 border-b border-b-slate-200 py-4">
+          <nav className="ml-4 pl-6">
+            <a href="#" className="hover:text-slate-600 cursor-pointer">
+              Home
+            </a>
+          </nav>
+        </div>
+      </header>
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
